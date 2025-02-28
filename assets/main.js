99b0ed8602c27b2c14ac63238108eb8d8524c4a9
alexciccarese <alex.ciccarese@gmail.com>
2025-02-28 12:51:16 +0100

    add foreach to take all elements from obj

--- assets/main.js
+++ assets/main.js
@@ -2,10 +2,17 @@ const polaroidEl = document.querySelector('.polaroid')
 
 fetch('https://lanciweb.github.io/demo/api/pictures/')
 .then(response => response.json())
-.then(data => {
+.then(pictures => {
 
-  const cardContent = data
+  const cardContent = pictures
   console.log(cardContent);
   
+
+  //prendiamo tutti gli elementi dell'oggetto
+  cardContent.forEach(pictures => {
+    console.log(pictures);
+
+    
+  });
 })
 .catch(error => console.error("Error:", error))
