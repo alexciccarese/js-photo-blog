ceb324b41d699352eedfc287140c5154cb74694c
alexciccarese <alex.ciccarese@gmail.com>
2025-03-03 15:06:11 +0100

    add img overlay in js

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,6 @@
 const polaroidEl = document.querySelector('.polaroid')
 const overlayEl = document.querySelector('.overlay')
+const overlayContentEl = document.querySelector('.overlay .container')
 const btnEl = document.querySelector('.btn')
 
 fetch('https://lanciweb.github.io/demo/api/pictures/')
@@ -12,8 +13,9 @@ fetch('https://lanciweb.github.io/demo/api/pictures/')
 
     //prendiamo tutti gli elementi dell'oggetto
     cardContent.forEach(pictures => {
+      const image = pictures.url
       console.log(pictures);
-      
+
       // aggiungiamo la nostra card con markup
       const markup =
         `
@@ -22,7 +24,7 @@ fetch('https://lanciweb.github.io/demo/api/pictures/')
             <div class="photo">
               <img style="width: 10%;" class="position-absolute top-0 start-50 translate-middle"
                 src="https://www.onlygfx.com/wp-content/uploads/2022/03/red-circle-round-3d-button-1.png" alt="">
-              <img src='${pictures.url}'
+              <img src='${image}'
                 class="img-fluid mb-3" alt="">
             </div>
             <p class="date m-0">${pictures.date}</p>
@@ -30,20 +32,28 @@ fetch('https://lanciweb.github.io/demo/api/pictures/')
           </div>
         </div>
           `
-
       polaroidEl.insertAdjacentHTML('beforeend', markup)
+
+      const markup_overlay =
+      `
+      <img class="mt-4 mb-4 col-12 col-lg-9" src="${image}" alt="">
+      `
+      overlayContentEl.insertAdjacentHTML('beforeend', markup_overlay)
+
     });
 
 
     // quando clicchiamo la card si apre l'overlay dell'IMG
-    polaroidEl.addEventListener('click', function() {
+    polaroidEl.addEventListener('click', function () {
       overlayEl.classList.remove('d-none')
     })
 
     // al click del bottone si chiude l'overlay
-    btnEl.addEventListener('click', function() {
+    btnEl.addEventListener('click', function () {
       overlayEl.classList.add('d-none')
     })
+
+    
 
 
   })
